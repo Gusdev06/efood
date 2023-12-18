073d4b2b89431cf0fd0bc45d0e6ddc81abc95218
Gusdev <coven688@gmail.com>
2023-12-18 11:59:12 -0300

    create cart

--- src/Containers/Perfil/bannerPerfil/index.tsx
+++ src/Containers/Perfil/bannerPerfil/index.tsx
@@ -1,13 +1,16 @@
+import { useSelector } from "react-redux";
 import banner from "../../../assets/Vector.png";
 import logo from "../../../assets/logo.svg";
+import { RootReducer } from "../../../store";
 import * as S from "./styles";
 const BannerPerfil = () => {
+  const { pratos } = useSelector((state: RootReducer) => state.cart);
   return (
     <S.HeaderProfile style={{ backgroundImage: `url(${banner})` }}>
       <S.Container>
         <p>Restaurantes</p>
         <S.LogoHeader src={logo} alt="efood" />
-        <p>0 Produto(S) no carrinho</p>
+        <p>{pratos.length} Produto(S) no carrinho</p>
       </S.Container>
     </S.HeaderProfile>
   );
